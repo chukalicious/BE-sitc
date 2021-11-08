1851b927498ae24dd963ab39c4eddf2ea525f6a2
Katiuska Alicea <katiuskaalicea@gmail.com>
2021-11-08 13:19:22 -0800

    added comments to the knex file as to insttuct on how to modify the knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -8,11 +8,14 @@ module.exports = {
       host: "localhost",
       port: 5432,
       user: "postgres",
+      // change this database to the one you will be using for your project
       database: "test_db",
       password: process.env.DB_PASSWORD,
     },
     migrations: {
+      // get rid of the contents of the data directory before using
       // location of migrations for the database
+
       directory: "./data/migrations",
     },
     seeds: {
